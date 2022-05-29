ae8de53e2eb792dce1abc441e2a3e9cbe1fef4a5
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-05-29 14:00:09 +0530

    added product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { ThankyouComponent } from './components/thankyou/thankyou.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
     RegisterComponent,
     LoginComponent,
     ThankyouComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
