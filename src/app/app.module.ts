fe2d4bda2b886187b7f9bea82ba3b8f0ab9992d3
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-05-29 13:57:27 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ThankyouComponent } from './components/thankyou/thankyou.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    CartComponent,
+    RegisterComponent,
+    LoginComponent,
+    ThankyouComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
